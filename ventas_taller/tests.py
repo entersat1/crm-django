5d70b4e1b3ac3f504baf11b69d4a39e82c3852fb
entersat1 <118567495+entersat1@users.noreply.github.com>
2025-10-31 22:21:56 -0300

    Delete ventas_taller directory

--- ventas_taller/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 221a76dd27c1c9ad53cabb1d52123a32be198d53
